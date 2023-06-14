bfcf59bee956e7b45af2078e97e8bbc6cbe75cd2
ksnt (takayuki kaisen) <ksn0215@gmail.com>
2023-06-14 20:07:42 +0900

    revised with eslinter

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-module.exports = function fizzbuzz(value)
-{
-  if (value % 15 == 0){
+module.exports = function fizzbuzz(value) {
+  if (value % 15 === 0) {
     return "FizzBuzz";
   }
-  if (value % 3 == 0){
+  if (value % 3 === 0) {
     return "Fizz";
   }
-  if (value % 5 == 0){
+  if (value % 5 === 0) {
     return "Buzz";
   }
-    return String(value);
+  return String(value);
 };
